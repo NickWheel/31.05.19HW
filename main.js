74f716aeca5d39fde601768f4f5291957eb4b6e2
NeKiTa <sungamma.99@gmail.com>
2019-05-31 16:04:10 +0300

    done!

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
 let btn = document.querySelector('.switch');
 
-
 let vertical_road = document.querySelector('.road_vertical');
 let vertical_lights = document.querySelectorAll('.lights_vertical');
 
@@ -12,38 +11,24 @@ let green_road = '#ABFFAB';
 let red_road = '#C95C5C';
 
 
-let counter = 0;
-console.log(counter);
-
-outer: for (let i=0;i<vertical_lights.length;i++) {
+for (let i=0;i<vertical_lights.length;i++) {
     btn.addEventListener('click', function() {
-        vertical_lights[i].style.backgroundColor = 'red';
-        vertical_road.style.zIndex = '4';
-        vertical_road.style.backgroundColor = red_road;
-
-        horizontal_lights[i].style.backgroundColor = 'lime';
-        horizontal_road.style.backgroundColor = green_road;
-        counter += 1;
-        console.log(counter, 'end of outer for loop', i);
-
         if(vertical_lights[i].style.backgroundColor === 'red') {
-            console.log(counter, 'in if', i);
-            btn.addEventListener('click', function() {
-                vertical_lights[i].style.backgroundColor = 'lime';
-                vertical_road.style.zIndex = '6';
-                vertical_road.style.backgroundColor = green_road;
-        
-                horizontal_lights[i].style.backgroundColor = 'red';
-                horizontal_road.style.backgroundColor = red_road;
-                counter += 1;
-                console.log(counter, 'before break', i);
-            });
+            vertical_lights[i].style.backgroundColor = 'lime';
+            vertical_road.style.zIndex = '6';
+            vertical_road.style.backgroundColor = green_road;
+
+            horizontal_lights[i].style.backgroundColor = 'red';
+            horizontal_road.style.backgroundColor = red_road;
+
+        }
+        else {
+            vertical_lights[i].style.backgroundColor = 'red';
+            vertical_road.style.zIndex = '4';
+            vertical_road.style.backgroundColor = red_road;
+
+            horizontal_lights[i].style.backgroundColor = 'lime';
+            horizontal_road.style.backgroundColor = green_road;
         }
     });
-    // i = 0;
-    
-    // btn.addEventListener('click', function() {
-    //     vertical_lights[i].style = 'background-color: lime;'
-    //     horizontal_lights[i].style = 'background-color: red;'
-    // });
 }
